0135702d638f198beaa9dbae31f0e027dd60ef72
Rodgers Ogada <ogadarodgers@gmail.com>
2022-12-03 04:40:37 +0300

    complete!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
+// the mockup provided is adding a list
+// anytime you click the submit -->Create new task<--- button
+// the lists created are also deletable when you click the X button
+// How do you conceptualize and execute this? 
+
+// First...
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const newTaskDescription = document.getElementById("new-task-description");
+// parent node
+const theList = document.getElementById("tasks");
+
+const createNewTask = event => { 
+  event.preventDefault(); 
+  // create a list element (li) and assign it to a variable
+  const newTask = document.createElement('li');
+
+  newTask.innerText = newTaskDescription.value;
+  createButton(newTask);
+  theList.appendChild(newTask);
+
+  event.target.reset();
+};
+
+const createButton = task => {
+   const btn = document.createElement('button');
+    btn.innerText = "X";
+
+    task.appendChild(btn);
+    btn.addEventListener('click',  (e) => deleteTask(e));
+}
+
+const deleteTask = (e) => {
+  const selectedTask = e.target.parentElement;
+  theList.removeChild(selectedTask);
+}
+
+document.querySelector('#create-task-form').addEventListener("submit"
+, (e) => createNewTask(e))
 });
